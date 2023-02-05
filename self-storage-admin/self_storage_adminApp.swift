55ec096f272b1d50ad9a5830cf2fbea50bc88c7d
Eddy Varela <eddvar@amazon.com>
2023-02-05 17:18:49 -0500

    Initial Commit

---
+++ self-storage-admin/self_storage_adminApp.swift
@@ -0,0 +1,17 @@
+//
+//  self_storage_adminApp.swift
+//  self-storage-admin
+//
+//  Created by Varela, Eddy on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct self_storage_adminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ self-storage-adminUITests/self_storage_adminUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  self_storage_adminUITestsLaunchTests.swift
+//  self-storage-adminUITests
+//
+//  Created by Varela, Eddy on 2/5/23.
+//
+
+import XCTest
+
+final class self_storage_adminUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
